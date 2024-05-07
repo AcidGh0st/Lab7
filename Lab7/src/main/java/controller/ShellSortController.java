1fad63d7d7b9945c86a2fa1f99f604f2a4b3e36c
Jimena <jimenaguevarach3@gmail.com>
2024-05-06 23:16:17 -0600

    Parte grafica del ShellSorting

---
+++ Lab7/src/main/java/controller/ShellSortController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class ShellSortController
+{
+    @javafx.fxml.FXML
+    private BorderPane borderPane;
+    @javafx.fxml.FXML
+    private TableView tableView_noSortedArray;
+    @javafx.fxml.FXML
+    private TextField txf_grapN2;
+    @javafx.fxml.FXML
+    private TableView tableView_SortedArray;
+    @javafx.fxml.FXML
+    private Button btn_Randomize;
+    @javafx.fxml.FXML
+    private Button btn_Start;
+    @javafx.fxml.FXML
+    private TextField txf_grapArray1;
+    @javafx.fxml.FXML
+    private TextField txf_grapArray3;
+    @javafx.fxml.FXML
+    private TextField txf_grapArray2;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void StartOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RandomizeOnAction(ActionEvent actionEvent) {
+    }
+}
